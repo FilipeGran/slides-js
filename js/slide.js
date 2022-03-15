2697588529adb1e3701da4084fa390165dafed15
Filipe Grangeiro <filipe2014grangeiro@gmail.com>
2022-03-14 22:28:29 -0300

    final da navagação prev-nev

--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,10 @@ export default class slide {
         }
     }
 
+    transition(active) {
+        this.slide.style.transition = active ? 'transform .3s' : '';
+    }
+
     moveSlide(distX) {
         this.dist.movePosition = distX;
         this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`
@@ -30,6 +34,7 @@ export default class slide {
             moveType = 'touchmove';
         }
         this.wrapper.addEventListener(moveType, this.onMove);
+        this.transition(false);
     }
 
     onMove(event) {
@@ -42,6 +47,18 @@ export default class slide {
         const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
         this.wrapper.removeEventListener(movetype, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;
+        this.changeSlideOnEnd();
+        this.transition(true);
+    }
+
+    changeSlideOnEnd() {
+        if (this.dist.movement > 120 && this.index.next !== undefined) {
+            this.activeNextSlide();
+        } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+            this.activePrevSlide();
+        } else {
+            this.changeSlide(this.index.active);
+        }
     }
 
     addSlideEvent() {
@@ -90,10 +107,23 @@ export default class slide {
         this.dist.finalPosition = activeSlide.position;
     }
 
+    activePrevSlide() {
+        if (this.index.prev !== undefined) {
+            this.changeSlide(this.index.prev);
+        }
+    }
+
+    activeNextSlide() {
+        if (this.index.next !== undefined) {
+            this.changeSlide(this.index.next);
+        }
+    }
+
     init() {
         this.bindEvents();
         this.addSlideEvent();
         this.slideConfig();
+        this.transition(true);
         return this;
     }
 }
